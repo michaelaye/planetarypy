8234debe4705f00e58e1a816807c9643cef9bcba
K.-Michael Aye <69774+michaelaye@users.noreply.github.com>
2025-03-27 17:30:14 +0100

    Refactor __init__.py to simplify imports and enhance module structure
    - Consolidate imports from archived_kernels, config, generic_kernels, and spicer using wildcard imports for cleaner code.
    - Update __all__ to dynamically include all exported names from the imported modules, improving maintainability and readability.

--- src/planetarypy/spice/__init__.py
+++ src/planetarypy/spice/__init__.py
@@ -4,74 +4,21 @@ This package provides tools for working with SPICE kernels and performing
 SPICE-related calculations for planetary science.
 """
 
-from .archived_kernels import (
-    Subsetter,
-    datasets,
-    get_metakernel_and_files,
-    is_start_valid,
-    is_stop_valid,
-    list_kernels_for_day,
-)
-from .config import (
-    BASE_URL,
-    KERNEL_STORAGE,
-    NAIF_URL,
-)
-from .generic_kernels import (
-    GENERIC_STORAGE,
-    GENERIC_URL,
-    download_generic_kernels,
-    generic_kernel_names,
-    generic_kernel_paths,
-    load_generic_kernels,
-    show_loaded_kernels,
-)
-from .spicer import (
-    EarthSpicer,
-    EnceladusSpicer,
-    IllumAngles,
-    Mars_Ls_now,
-    MarsSpicer,
-    MoonSpicer,
-    PlutoSpicer,
-    Radii,
-    Spicer,
-    SurfaceCoords,
-    TritonSpicer,
-    make_axis_rotation_matrix,
-)
+from .archived_kernels import *
+from .config import *
+from .generic_kernels import *
+from .spicer import *
 
-__all__ = [
+__all__ = (
     # From archived_kernels
-    "datasets",
-    "is_start_valid",
-    "is_stop_valid",
-    "Subsetter",
-    "get_metakernel_and_files",
-    "list_kernels_for_day",
+    archived_kernels.__all__
+    +
     # From config
-    "KERNEL_STORAGE",
-    "NAIF_URL",
-    "BASE_URL",
+    config.__all__
+    +
     # From generic_kernels
-    "GENERIC_STORAGE",
-    "GENERIC_URL",
-    "generic_kernel_names",
-    "generic_kernel_paths",
-    "download_generic_kernels",
-    "load_generic_kernels",
-    "show_loaded_kernels",
+    generic_kernels.__all__
+    +
     # From spicer
-    "Radii",
-    "make_axis_rotation_matrix",
-    "IllumAngles",
-    "SurfaceCoords",
-    "Spicer",
-    "MarsSpicer",
-    "TritonSpicer",
-    "EnceladusSpicer",
-    "PlutoSpicer",
-    "EarthSpicer",
-    "MoonSpicer",
-    "Mars_Ls_now",
-]
+    spicer.__all__
+)
