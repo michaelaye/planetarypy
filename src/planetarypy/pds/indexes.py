0a9fe95b92402dc28d021dabb9c2445a517e8bc7
K.-Michael Aye <69774+michaelaye@users.noreply.github.com>
2025-10-01 22:31:11 +0200

    remove fastcore dependency

--- src/planetarypy/pds/indexes.py
+++ src/planetarypy/pds/indexes.py
@@ -3,22 +3,19 @@
 __all__ = ["logger", "storage_root", "dynamic_urls", "Index"]
 
 import datetime  # Use absolute import instead of from datetime import datetime
-import logging
+from pathlib import Path
 from urllib.request import URLError
 
 import pandas as pd
-from fastcore.basics import patch  # better monkeypatcher
-from fastcore.xtras import Path  # improved pathlib.Path
+from loguru import logger
 from yarl import URL
 
 from .. import utils
 from ..config import config
 from .ctx_index import CTXIndex
-from .index_config import access_log, discover_dynamic_urls, get_url
+from .index_config import IndexConfigManager, access_log
 from .index_labels import IndexLabel
 from .lroc_index import LROCIndex
-
-logger = logging.getLogger(__name__)
 
 storage_root = Path(config.storage_root)
 
@@ -37,7 +34,7 @@ class Index:
         url: str = None,  # URL to index. If not given, will be read from config object.
         check_update: bool = True,  # switch if dynamic URLS will be derived upon start
     ):
-        self.key = self.parse_key(key)
+        self.key = key
         self.check_update = check_update
 
         self.set_url(url)
@@ -54,16 +51,14 @@ class Index:
 
         self._remote_timestamp = None
 
-    def set_url(self, url):  # URL to index.
+    def set_url(self, url):
         """Set URL from having it dynamically determined (for non-static index URLs)."""
-        self.url = get_url(self.key) if url is None else url
-        if not self.url and self.check_update:  # empty ''
+        manager = IndexConfigManager()
+        self.url = manager.get_url(self.key) if url is None else url
+        if not self.url and self.check_update and self.instrument_key in dynamic_urls:
             self.url = dynamic_urls[self.instrument_key]().latest_index_label_url
-            # Store the dynamically determined URL in the config
             if self.url:
-                from .index_config import set_url as config_set_url
-
-                config_set_url(self.key, self.url)
+                manager.set_url(self.key, str(self.url))
 
     @property
     def isotimestamp(self):
@@ -87,19 +82,15 @@ class Index:
 
     @property
     def mission(self):
-        return self.key_tokens[1]
-
-    @property
-    def mission_key(self):
-        return ".".join(self.key_tokens[1:2])
+        return self.key_tokens[0]
 
     @property
     def instrument(self):
-        return self.key_tokens[2]
+        return self.key_tokens[1]
 
     @property
     def instrument_key(self):
-        return ".".join(self.key_tokens[1:3])
+        return ".".join(self.key_tokens[0:2])
 
     @property
     def index_name(self):
@@ -111,7 +102,14 @@ class Index:
         if self.url:
             return Path(str(self.url).split("/")[-1])
         else:
-            return Path.ls(self.local_dir, file_exts=[".lbl", ".LBL"])[0]
+            # Find label files using Path.glob()
+            label_files = list(self.local_dir.glob("*.lbl")) + list(
+                self.local_dir.glob("*.LBL")
+            )
+            if label_files:
+                return label_files[0].name
+            else:
+                raise FileNotFoundError(f"No label files found in {self.local_dir}")
 
     @property
     def isupper(self):
@@ -188,104 +186,82 @@ class Index:
     def __repr__(self):
         return self.__str__()
 
+    def download(
+        self,
+        convert_to_parquet: bool = True,  # set to False if you just want download the files
+    ):
+        """Wrapping URLs for downloading PDS indices and their label files."""
+        # check timestamp
+        label_url = self.url
+        logger.info("Downloading %s." % label_url)
+        utils.url_retrieve(label_url, self.local_label_path)
+        logger.info("Downloading %s.", self.table_url)
+        utils.url_retrieve(self.table_url, self.local_table_path)
+        print(f"Downloaded {self.local_label_path} and {self.local_table_path}")
+        # if (
+        #     self.key == "mro.hirise.indexes.edr"
+        # ):  # HiRISE EDR index is broken on the PDS. Team knows.
+        #     print("Fixing broken EDR index...")
+        #     fix_hirise_edrcumindex(
+        #         self.local_table_path, self.local_table_path.with_name("temp.tab")
+        #     )
+        #     self.local_table_path.with_name("temp.tab").rename(self.local_table_path)
+        self.timestamp = self.remote_timestamp
+        self.update_timestamp()
+        if convert_to_parquet:
+            self.convert_to_parquet()
 
-@patch
-def parse_key(
-    self: Index,
-    key: str,  # dotted key
-):
-    """Take care of different ways how the key could be structured.
+    @property
+    def update_available(self) -> bool:  # Boolean indicating if there's a new index
+        "Property indicating if the index needs to be downloaded."
+        if not self.timestamp:
+            return True  # never downloaded
+        return True if self.remote_timestamp > self.timestamp else False
 
-    This is a simplified version that no longer adds the 'indexes' subkey,
-    as we've simplified the structure for clarity.
-    """
-    # If key already starts with "missions", keep it as is
-    if key.startswith("missions"):
-        return key
-    # Otherwise, add the "missions" prefix
-    return "missions." + key
+    # Add a new method to provide user-friendly update notifications
+    def check_for_updates(
+        self,
+        print_result: bool = True,  # Whether to print user-friendly messages
+    ):
+        """Check if updates are available for this index and optionally print notifications.
 
+        Args:
+            print_result: Whether to print user-friendly messages about available updates
 
-@patch
-def download(
-    self: Index,  # the Index object defined in this module
-    convert_to_parquet: bool = True,  # set to False if you just want download the files
-):
-    """Wrapping URLs for downloading PDS indices and their label files."""
-    # check timestamp
-    label_url = self.url
-    logger.info("Downloading %s." % label_url)
-    utils.url_retrieve(label_url, self.local_label_path)
-    logger.info("Downloading %s.", self.table_url)
-    utils.url_retrieve(self.table_url, self.local_table_path)
-    print(f"Downloaded {self.local_label_path} and {self.local_table_path}")
-    # if (
-    #     self.key == "missions.mro.hirise.indexes.edr"
-    # ):  # HiRISE EDR index is broken on the PDS. Team knows.
-    #     print("Fixing broken EDR index...")
-    #     fix_hirise_edrcumindex(
-    #         self.local_table_path, self.local_table_path.with_name("temp.tab")
-    #     )
-    #     self.local_table_path.with_name("temp.tab").rename(self.local_table_path)
-    self.timestamp = self.remote_timestamp
-    self.update_timestamp()
-    if convert_to_parquet:
-        self.convert_to_parquet()
+        Returns:
+            dict: Information about available updates
+        """
+        result = {"has_update": False, "details": {}}
 
+        # Check if this is a dynamic URL that might have a newer index location
+        if self.instrument_key in dynamic_urls:
+            # Discover the latest URLs
+            manager = IndexConfigManager()
+            updates = manager.discover_dynamic_urls()
+            # Find if this index has an update
+            key = f"{self.instrument_key}.{self.index_name}"
+            if key in updates and updates[key].get("is_update", False):
+                result["has_update"] = True
+                result["details"]["url_update"] = updates[key]
 
-@patch(as_prop=True)
-def update_available(self: Index) -> bool:  # Boolean indicating if there's a new index
-    "Property indicating if the index needs to be downloaded."
-    if not self.timestamp:
-        return True  # never downloaded
-    return True if self.remote_timestamp > self.timestamp else False
-
-
-# Add a new patch method to provide user-friendly update notifications
-@patch
-def check_for_updates(
-    self: Index,
-    print_result: bool = True,  # Whether to print user-friendly messages
-):
-    """Check if updates are available for this index and optionally print notifications.
-
-    Args:
-        print_result: Whether to print user-friendly messages about available updates
-
-    Returns:
-        dict: Information about available updates
-    """
-    result = {"has_update": False, "details": {}}
-
-    # Check if this is a dynamic URL that might have a newer index location
-    if self.instrument_key in dynamic_urls:
-        # Discover the latest URLs
-        updates = discover_dynamic_urls()
-
-        # Find if this index has an update
-        key = f"{self.instrument_key}.{self.index_name}"
-        if key in updates and updates[key].get("is_update", False):
+        # Check for timestamp update
+        if self.update_available:
             result["has_update"] = True
-            result["details"]["url_update"] = updates[key]
+            result["details"]["timestamp_update"] = {
+                "current": self.timestamp.isoformat() if self.timestamp else None,
+                "remote": self.remote_timestamp.isoformat()
+                if self.remote_timestamp
+                else None,
+            }
 
-    # Check for timestamp update
-    if self.update_available:
-        result["has_update"] = True
-        result["details"]["timestamp_update"] = {
-            "current": self.timestamp.isoformat() if self.timestamp else None,
-            "remote": self.remote_timestamp.isoformat()
-            if self.remote_timestamp
-            else None,
-        }
+            # Print notification about timestamp update
+            if print_result and self.timestamp:
+                print(f"Update available: Newer version of {self.key} index found!")
+                print(f"  Current version: {self.timestamp.isoformat()}")
+                print(f"  Available version: {self.remote_timestamp.isoformat()}")
 
-        # Print notification about timestamp update
-        if print_result and self.timestamp:
-            print(f"Update available: Newer version of {self.key} index found!")
-            print(f"  Current version: {self.timestamp.isoformat()}")
-            print(f"  Available version: {self.remote_timestamp.isoformat()}")
+        # Print if no updates were found
+        if print_result and not result["has_update"]:
+            print(f"No updates available for {self.key}")
 
-    # Print if no updates were found
-    if print_result and not result["has_update"]:
-        print(f"No updates available for {self.key}")
-
-    return result
+        return result
