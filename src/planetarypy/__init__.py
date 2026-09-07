1a360184a65819e7b0bbeeb14ca45228440e694a
K.-Michael Aye <69774+michaelaye@users.noreply.github.com>
2026-09-07 10:14:55 +0200

    Bump version: 0.82.1 → 0.83.0

--- src/planetarypy/__init__.py
+++ src/planetarypy/__init__.py
@@ -11,7 +11,7 @@ __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
 __author__ = """PlanetaryPy Developers"""
 __email__ = "kmichael.aye@gmail.com"
-__version__ = "0.82.1"
+__version__ = "0.83.0"
 
 __all__ = [
     "enable_logging",
